e966ed899f59d1e38512066f99de711bf6b39ddf
f-frhs <top.tuna+github@gmail.com>
2021-11-07 16:08:08 +0900

    Add: ShippingCalculatorTests

---
+++ ShoppingCartServiceTests/BusinessLogic/ShippingCalculatorTests.cs
@@ -0,0 +1,9 @@
+﻿using ShoppingCartService.BusinessLogic;
+
+namespace ShoppingCartServiceTests.BusinessLogic
+{
+    public class ShippingCalculatorTests
+    {
+        // shippingCalculator.CalculateShippingCost() is already tested in CheckOutEngineTests.
+    }
+}
