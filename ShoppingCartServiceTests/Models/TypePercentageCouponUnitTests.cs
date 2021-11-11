7aa146840bee6a11095af85ae7dde4c5fa325331
f-frhs <top.tuna+github@gmail.com>
2021-11-11 14:53:00 +0900

    Refactor: split tests for Coupon
    - TypeAbsoluteCouponUnitTests
    - TypePercentageCouponUnitTests

--- ShoppingCartServiceTests/Models/TypePercentageCouponUnitTests.cs
+++ ShoppingCartServiceTests/Models/TypePercentageCouponUnitTests.cs
@@ -4,26 +4,6 @@ using Xunit;
 
 namespace ShoppingCartServiceTests.Models
 {
-    public class TypeAbsoluteCouponUnitTests
-    {
-        [InlineData(0)]
-        [InlineData(1)]
-        [InlineData(1.1)]
-        [Theory]
-        public void Ctor_ValidAmount_Pass(double amount)
-        {
-            new TypeAbsoluteCoupon(amount);
-        }
-
-        [InlineData(-1.1)]
-        [InlineData(-1)]
-        [Theory]
-        public void Ctor_InvalidAmount_ThrowArgumentOutOfRangeException(double amount)
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() => new TypeAbsoluteCoupon(amount));
-        }
-    }
-
     public class TypePercentageCouponUnitTests
     {
         [InlineData(0)]
